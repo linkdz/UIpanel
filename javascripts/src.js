97882c1a15959592a4411bb93cfeddd28d629dd6
Rachid Slimani <racslim@mail.com>
2016-04-15 19:09:58 +0100

    Update src.js

--- javascripts/src.js
+++ javascripts/src.js
@@ -1,5 +1,12 @@
-function changeSource() {
-    var image = document.querySelectorAll("img")[0];
-    var source = image.src = image.src.replace("/images/icon-beginhere.gif","01.png");
+function removejscssfile(filename, filetype){
+    var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
+    var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
+    var allsuspects=document.getElementsByTagName(targetelement)
+    for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+    if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
+        allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+    }
 }
-changeSource();
+ 
+removejscssfile("somescript.js", "js") //remove all occurences of "somescript.js" on page
+removejscssfile("combined_optimized.css", "css") //remove all occurences "somestyle.css" on page
