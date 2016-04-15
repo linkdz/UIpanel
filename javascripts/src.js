af0447cc9794581506b44413038a3c0c2552c587
Rachid Slimani <racslim@mail.com>
2016-04-15 18:24:22 +0100

    Update src.js

--- javascripts/src.js
+++ javascripts/src.js
@@ -1,1 +1,8 @@
+function changeSource() {
 
+    var image = document.querySelectorAll("img");
+    var source = image.getAttribute("src").replace("placeHolder.png", "01.png");
+    image.setAttribute("src", source);
+}
+
+changeSource();
